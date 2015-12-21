0d2f549d9feead22ce89590d8fb5f1f8be7fc078
Yanick Champoux <yanick@cpan.org>
2015-12-21 17:42:20 -0500

    first commit

---
+++ lib/MooseX/Storage/Base/SerializedClass.pm
@@ -0,0 +1,99 @@
+package MooseX::Storage::Base::SerializedClass;
+# ABSTRACT: Deserialize according to the serialized __CLASS__
+
+=head1 SYNOPSIS
+
+  package ThirdDimension;
+  use Moose::Role;
+
+  has 'z' => (is => 'rw', isa => 'Int');
+
+  package Point;
+  use Moose;
+  use MooseX::Storage;
+
+  with Storage( base => 'SerializedClass', traits => [ 'WithRoles' ] );
+
+  has 'x' => (is => 'rw', isa => 'Int');
+  has 'y' => (is => 'rw', isa => 'Int');
+
+  1;
+
+  use Moose::Util qw/ with_traits /;
+
+  my $p = with_traits( 'Point', 'ThirdDimension' )->new(x => 10, y => 10, z => 10);
+
+  my $packed = $p->pack(); 
+  # { __CLASS__ => 'Point', '__ROLES__' => [ 'ThirdDimension' ], x => 10, y => 10, z => 10 }
+
+  # unpack the hash into a class
+  my $p2 = Point->unpack($packed);
+
+  print $p2->z;
+
+=head1 DESCRIPTION
+
+Behaves like L<MooseX::Storage::Basic>, with the exception that 
+the unpacking will reinflate the object into the class and roles
+as provided in the serialized data. It is means to be used in
+conjuncture with L<MooseX::Storage::Traits::WithRoles>.
+
+=head1 EXPORTED FUNCTIONS
+
+The function C<moosex_unpack> can be exported. The function unpacks
+a serialized object based on its C<__CLASS__> and C<__ROLES__> attributes.
+
+    use MooseX::Storage::Base::SerializedClass qw/ moosex_unpack /;
+
+    my $object = moosex_unpack( $struct );
+
+=cut
+
+use parent 'Exporter';
+
+use Moose::Role;
+
+with 'MooseX::Storage::Basic';
+
+use Moose::Util qw/ with_traits /;
+use Class::Load 'load_class';
+
+our @EXPORT_OK = qw/ moosex_unpack /;
+
+use namespace::autoclean;
+
+around unpack => sub {
+    my( $orig, $class, $data, %args ) = @_;
+    $class = Class::Load::load_class( $data->{'__CLASS__'} );
+
+    if( my $roles = delete $data->{'__ROLES__'} ) {
+        $class = with_traits( $class, @$roles );
+        $data->{'__CLASS__'} = $class;
+    }
+
+    $orig->($class,$data,%args);
+};
+
+sub moosex_unpack {
+    MooseX::Storage::Base::SerializedClass::Dummy->unpack(shift);
+}
+
+
+{
+    package 
+        MooseX::Storage::Base::SerializedClass::Dummy;
+
+    use Moose;
+    use MooseX::Storage;
+
+    with Storage( base => 'SerializedClass', traits => [ 'WithRoles' ] );
+
+    __PACKAGE__->meta->make_immutable;
+
+}
+
+1;
+
+
+
+
--- lib/MooseX/Storage/Traits/WithRoles.pm
+++ lib/MooseX/Storage/Traits/WithRoles.pm
@@ -1,13 +1,72 @@
 package MooseX::Storage::Traits::WithRoles;
-# ABSTRACT: 
+# ABSTRACT: A custom trait to include roles in serialization
+
+use Moose::Role;
+use namespace::autoclean;
+
+requires 'pack';
+requires 'unpack';
+
+around 'pack' => sub {
+    my ($orig, $self, %args) = @_;
+    $args{engine_traits} ||= [];
+    push(@{$args{engine_traits}}, 'WithRoles');
+    $self->$orig(%args);
+};
+
+around 'unpack' => sub {
+    my ($orig, $self, $data, %args) = @_;
+    $args{engine_traits} ||= [];
+    push(@{$args{engine_traits}}, 'WithRoles');
+    $self->$orig($data, %args);
+};
+
+no Moose::Role;
+
+1;
+
+__END__
 
 =head1 SYNOPSIS
 
+  package ThirdDimension;
+  use Moose::Role;
+
+  has 'z' => (is => 'rw', isa => 'Int');
+
+  package Point;
+  use Moose;
+  use MooseX::Storage;
+
+  with Storage( base => 'SerializedClass', traits => [ 'WithRoles' ] );
+
+  has 'x' => (is => 'rw', isa => 'Int');
+  has 'y' => (is => 'rw', isa => 'Int');
+
+  1;
+
+  use Moose::Util qw/ with_traits /;
+
+  my $p = with_traits( 'Point', 'ThirdDimension' )->new(x => 10, y => 10, z => 10);
+
+  my $packed = $p->pack(); 
+  # { __CLASS__ => 'Point', '__ROLES__' => [ 'ThirdDimension' ], x => 10, y => 10, z => 10 }
+
+  # unpack the hash into a class
+  my $p2 = Point->unpack($packed);
+
+  print $p2->z;
+
 =head1 DESCRIPTION
 
+This trait is meant to be used when a base class will be consuming roles at runtime
+via (for example) C<with_traits>.
+Without this trait, the '__CLASS__' attribute of the serialized object would be the name
+of the resulting anonymous class, which is useless to reconstruct the class after the fact.
+
+When this trait is used, the serialized C<__CLASS__> value will be the base 
+class, and C<__ROLES__> will contain the list of roles that it consumes. If used
+in conjecture with L<MooseX::Storage::Base::SerializedClass>, C<unpack()> will reinflate the data
+in the right class augmented by the given roles.
+
 =cut
-
-use strict;
-use warnings;
-
-1;
